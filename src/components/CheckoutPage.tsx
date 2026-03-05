26a0983f57011332585c22aa36b84dd83050f4c1
Pxiskkj <spotifypayoficial@gmail.com>
2026-03-05 10:45:54 -0300

    Update CheckoutPage.tsx

--- src/components/CheckoutPage.tsx
+++ src/components/CheckoutPage.tsx
@@ -1,4 +1,4 @@
 function handleSubmit() {
     // Redirect to the external checkout URL instead of using the onContinue callback
-    window.location.href = 'https://pagamento.econoflexbrasil.club/checkout?product=e00b6a4a-1827-11f1-b2a5-46da4690ad53';
+    window.location.href = 'https://pagamento.econoflexbrasil.club/checkout?product=16ffbdf7-185c-11f1-b2a5-46da4690ad53';
 }
