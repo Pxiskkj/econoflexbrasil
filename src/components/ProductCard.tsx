93b3c2c1d72a3707301b811ee3a01d4aea4a6d3d
Pxiskkj <spotifypayoficial@gmail.com>
2026-03-04 21:19:50 -0300

    Update ProductCard.tsx

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -5,9 +5,9 @@ interface ProductCardProps {
 }
 
 const ProductCard = ({ onClick }: ProductCardProps) => {
-  const originalPrice = 498;
-  const currentPrice = 127.42;
-  const pixPrice = 114.68;
+  const originalPrice = 187;
+  const currentPrice = 76.00;
+  const pixPrice = 68.40;
   const discount = Math.round(((originalPrice - currentPrice) / originalPrice) * 100);
   const installments = (currentPrice / 6).toFixed(2);
 
